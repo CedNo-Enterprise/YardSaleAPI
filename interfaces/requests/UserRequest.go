1da53b5fe796ec522f093b11e3766dd6a23b0e47
Cedric <cedric.noel.2@ulaval.ca>
2026-07-26 15:34:02 -0400

    Constrained password field to only printable ascii

--- interfaces/requests/UserRequest.go
+++ interfaces/requests/UserRequest.go
@@ -2,6 +2,6 @@ package requests
 
 type UserRequest struct {
 	Username string `json:"username"   validate:"required,min=3,max=15,alphanum"`
-	Password string `json:"password"   validate:"required,min=12,max=64"`
+	Password string `json:"password"   validate:"required,min=12,max=64,printascii"`
 	Email    string `json:"email"      validate:"required,email"`
 }
